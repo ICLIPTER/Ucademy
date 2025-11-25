3f7a46cab416ad14fef6c5a2325b7c2a8b5207aa
Bibek <bibekreigns5278@gmail.com>
2025-11-25 15:06:21 +0530

    23: Implementing Progress Tracking

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { PublicCoursesCard } from "../(public)/_components/PublicCourseCard";
 import { EmptyState } from "../admin/courses/_components/general/EmptyState";
 import { getAllCourses } from "../data/course/get-all-courses";
 import { getEnrolledCourses } from "../data/user/get-enrolled-courses";
+import { CourseProgressCard } from "./_components/CourseProgressCard";
 
 export default async function DashboardPage() {
   const [courses, enrolledCourses] = await Promise.all([
@@ -29,12 +30,7 @@ export default async function DashboardPage() {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {enrolledCourses.map((course) => (
-            <Link
-              key={course.course.id}
-              href={`/dashboard/${course.course.slug}`}
-            >
-              {course.course.title}
-            </Link>
+           <CourseProgressCard key={course.course.id} data={course} />
           ))}
         </div>
       )}
